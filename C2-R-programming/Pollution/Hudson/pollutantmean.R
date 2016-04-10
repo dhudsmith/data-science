8e8c7a41e966de00b0fc4b7fb609ab1c7e61d08c
dhudsmith <dhudsmith@gmail.com>
2016-04-09 21:51:02 -0400

    Started working on part 1

--- C2-R-programming/Pollution/Hudson/pollutantmean.R
+++ C2-R-programming/Pollution/Hudson/pollutantmean.R
@@ -1,3 +1,19 @@
+library(readr)
+
+#Part 1
 pollutantmean <- function(directory, pollutant, id = 1:322){
+  # Get the files
+  files.ls <- list.files(directory)[id]
   
+  # Calculate the mean
+  pollutant.vector <- vapply(files.ls, 
+                             function(x){
+                               full_path <- file.path(directory, x)
+                               df <- read_csv(full_path)
+                               return( df[,pollutant])
+                               },
+                             FUN.VALUE = numeric(1)
+                             )
+  return(pollutant.vector)
+  return(mean(pollutant.vector, na.rm = T))
 }
