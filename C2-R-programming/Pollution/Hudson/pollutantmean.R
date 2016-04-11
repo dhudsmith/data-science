79d2a0bcc1f9fc5d3f24224b54333c7ec6d88ef1
dhudsmith <dhudsmith@gmail.com>
2016-04-10 21:46:24 -0400

    Finished part 2, Finished part 3, but numbers differ slightly from examples. Perhaps options in cor funciton.

--- C2-R-programming/Pollution/Hudson/pollutantmean.R
+++ C2-R-programming/Pollution/Hudson/pollutantmean.R
@@ -22,3 +22,48 @@ pollutantmean <- function(directory, pollutant, id = 1:322){
   # Calculate the mean
   sum(pollutant.vec[1,])/sum(pollutant.vec[2,])
 }
+
+# Part 2
+complete <- function(directory, id = 1:322){
+  # Get the files
+  files.ls <- list.files(directory)[id]
+  
+  # Get the sum of pollutant and number of observations for each id
+  nobs.vec <- vapply(files.ls, 
+                          function(x){
+                            full_path <- file.path(directory, x)
+                            df <- read_csv(full_path)
+                            
+                            # Return the total and the number of complete observations
+                            unname( sum(!is.na(df$sulfate) & !is.na(df$nitrate)))
+                          },
+                          FUN.VALUE = numeric(1)
+                     )
+  
+  # Convert to data.frame
+  data.frame(id = id, nobs = nobs.vec, row.names = NULL)
+}
+
+# Part 3
+corr <- function(directory, threshold = 0){
+  # Get the files
+  files.ls <- list.files(directory)
+  
+  # Get the file ids that meet the threshold
+  files.ls <- files.ls[ subset( complete(directory), nobs >= threshold )$id ]
+  
+  # Get the sum of pollutant and number of observations for each id
+  cor.vec <- vapply(files.ls, 
+                     function(x){
+                       full_path <- file.path(directory, x)
+                       df <- read_csv(full_path)
+                       
+                       # Return the correlations
+                       cor(df$sulfate, df$nitrate, use = "pairwise.complete.obs")
+                     },
+                     FUN.VALUE = numeric(1)
+  )
+  
+  # Convert to data.frame
+  unname( cor.vec )
+}
